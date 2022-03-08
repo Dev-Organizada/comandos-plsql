1b061a99313fa7a2428980ce1f3187fc20e2b93d
Nathaly Rodrigues <100220606+Dev-Organizada@users.noreply.github.com>
2022-03-08 15:22:00 -0300

    Delete Lista de tarefas directory

--- Lista de tarefas/Criacao da tabela.sql
+++
@@ -1,7 +0,0 @@
-create table tb_lista_de_tarefas(
-       id_lista      number not null constraint pk_lista_tarefa primary key,
-       descricao     varchar2(400) not null,
-       dt_realizacao date,
-       fl_status     number default 1,
-       dt_registro   date   default sysdate
-);

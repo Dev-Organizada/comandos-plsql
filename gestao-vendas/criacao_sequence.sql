4272d832d2224eb012d787a42b34ff022484352e
Nathaly Rodrigues <100220606+Dev-Organizada@users.noreply.github.com>
2025-02-06 22:54:17 -0300

    Create criacao_sequence.sql

---
+++ gestao-vendas/criacao_sequence.sql
@@ -0,0 +1,71 @@
+create sequence categoria_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence cidade_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence cliente_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence endereco_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence estoque_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence pagamento_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence pedido_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence item_pedido_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
+
+create sequence produto_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocache
+cycle;
